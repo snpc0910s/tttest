c1168c2b89c61ae1a870cd4a0877dce2ce2ef5fb
snpc0910s <snpc0910@gmail.com>
2024-06-30 14:18:01 +0700

    keke

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
-        System.out.println("Change A");
-        System.out.println("Hard to A");
+        System.out.println("Keke");
     }
 }
