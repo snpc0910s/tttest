3d25df224915708f6267e0e4386637fd32d596df
snpc0910s <snpc0910@gmail.com>
2024-06-30 14:11:43 +0700

    reset to B

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
-        System.out.println("Change AB");
         System.out.println("Change B");
+        System.out.println("change C");
     }
 }
